4674265585c6a144384f476a96cfce77136bbb13
Eric Lathrop <eric@ericlathrop.com>
2016-05-03 18:00:08 -0400

    Normalize path separators so it works on windows

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,29 @@
+var path = require("path");
 var webpack = require("webpack");
 
 /* eslint-disable no-undef */
 var environment = process.env["NODE_ENV"] || "development";
+
+function escapeForRegExp(s) {
+  return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+}
+
+function pathForRegExp(unsafePath) {
+  var normalized = path.resolve(__dirname, unsafePath);
+  return escapeForRegExp(normalized);
+}
+
+var imagePath = pathForRegExp("src/images/");
+var imageRegExp = new RegExp(imagePath + ".*\\.(jpe?g|png|gif|svg)$", "i");
+
+var fontPath = pathForRegExp("src/fonts/");
+var fontRegExp = new RegExp(fontPath + ".*\.(eot|svg|ttf|woff2?)$", "i");
+
+var soundPath = pathForRegExp("src/sounds/");
+var soundRegExp = new RegExp(soundPath + ".*\\.(mp3|ogg|wav)$", "i");
+
+var htmlPath = pathForRegExp("src/index.html");
+var htmlRegExp = new RegExp(htmlPath, "i");
 
 module.exports = {
   entry: "./src/game",
@@ -16,22 +38,22 @@ module.exports = {
     loaders: [
       { test: /\.json$/, loader: "json" },
       {
-        test: /src\/images\/.*\.(jpe?g|png|gif|svg)$/i,
+        test: imageRegExp,
         loaders: [
           "file?hash=sha512&digest=hex&name=images/[name].[ext]"
           //"image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false"
         ]
       },
       {
-        test: /src\/fonts\/.*\.(eot|svg|ttf|woff2?)$/i,
-        loader: "file?hash=sha512&digest=hex&name=fonts/[name].[ext]"
+        test: fontRegExp,
+        loader: "file?hash=sha513&digest=hex&name=fonts/[name].[ext]"
       },
       {
-        test: /src\/sounds\/.*\.(mp3|ogg|wav)$/i,
+        test: soundRegExp,
         loader: "file?hash=sha512&digest=hex&name=sounds/[name].[ext]"
       },
       {
-        test: /src\/index.html$/i,
+        test: htmlRegExp,
         loader: "file?hash=sha512&digest=hex&name=[name].[ext]"
       }
     ]
